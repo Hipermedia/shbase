efe079eda9f2a045407678ecf2e9e81cb95e3341
Alejandro Sobrevilla <alejandro@sobrevilla.mx>
2015-03-23 19:41:53 -0600

    Quité el contente de la plantilla acordeon

--- sh-page-acordeon.php
+++ sh-page-acordeon.php
@@ -25,7 +25,40 @@ get_header(); ?>
                 </section><!-- .entry-content -->	
                
                 <section class="entry-acordeon">
-					<?php get_template_part( 'content', 'acordeon-acf' ); ?>
+					<?php if(get_field('acordeon')): ?>
+                        <?php $num_titulo = 1; ?>
+                        <?php $num_contenido = 1; ?>
+                        <?php while(has_sub_field('acordeon')): ?>
+                            <section class="bloque-acordeon">
+                                <h3 id="titulo-acordeon<?php echo $num_titulo++; ?>" class="titulo-acordeon fa fa-plus-square"><span><?php the_sub_field('titulo_de_la_pestana'); ?></span></h3>
+                                <div id="contenido-acordeon<?php echo $num_contenido++; ?>" class="contenido-acordeon entry-content"><?php the_sub_field('contenido_de_la_pestana'); ?></div>
+                            </section>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+
+                            <script>
+                            jQuery(document).ready(function($) {
+                                $(".contenido-acordeon").hide();
+                                <?php if(get_field('acordeon')): ?>
+                                    <?php $cuentita = 1; ?>
+                                    <?php while(has_sub_field('acordeon')): ?>
+                                        var on<?php echo $cuentita; ?> = false;
+                                        $('h3#titulo-acordeon'+<?php echo $cuentita; ?>).click(function () {
+                                            $('div#contenido-acordeon'+<?php echo $cuentita; ?>).toggle('slow');
+                                            var d = document.getElementById("titulo-acordeon<?php echo $cuentita; ?>");
+                                            if(on<?php echo $cuentita; ?>) {
+                                                d.className = "titulo-acordeon fa fa-plus-square";
+                                                 on<?php echo $cuentita; ?> = false;
+                                            } else {
+                                                d.className = "titulo-acordeon fa fa-minus-square";
+                                                on<?php echo $cuentita; ?> = true;
+                                            }
+                                        });
+                                    <?php $cuentita++; ?>
+                                    <?php endwhile; ?>
+                                <?php endif; ?>
+                            });
+                            </script>
                 </section>
     			
                 <footer class="entry-footer">
